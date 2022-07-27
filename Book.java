dcda959402c1a63d396977c4df3ac5ddbcf534b9
Janna Lin <archico@dhcp-vl2041-2847.eduroam.cornell.edu>
2022-07-27 13:17:53 -0400

    Fix example title typo

--- Book.java
+++ Book.java
@@ -1,20 +1,22 @@
 /** A class modeling a book. */
 public class Book {
     String title;
+    String author;
 
     /** Create a new book with title b. */
-    public Book(String t) {
-        title= t;
+    public Book(String title, String author) {
+        this.title= title;
+	this.author= author;
     }
 
     /** Print this book. */
     public void printBook() {
-        System.out.println(title);
+        System.out.println(title+" "+author);
     }
 
     /** Some book examples. */
     public static void main(String[] args) {
-        Book b = new Book("The Clockwork Orange");
+        Book b = new Book("Gone With the Wind", "Margaret Mitchell");
         b.printBook();
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 An example git repository for CS 2110.# cs2110lab7
+The book object includes a String title and a String author.
